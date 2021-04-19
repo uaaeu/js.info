048253295ef58095a77cbe3192b1cd7e6380fdf7
uaaeu <yurtmusa@gmail.com>
2021-04-19 16:29:20 +0300

    variables

--- alert.js
+++ alert.js
@@ -1,5 +1,24 @@
+'use strict';
+
 // one line comment
 alert('hello i am javascript!');
 /* multiple line
 comment
 */
+
+let admin;
+let user;
+
+user = 'musa';
+
+admin = user;
+
+alert(admin);
+
+let ourPlanetName = 'earth';
+
+let currentUserName = 'uaaeu';
+
+// constants
+const BIRTHDATE = '13.12.1988';
+// const age = someCode(BIRTHDATE);
