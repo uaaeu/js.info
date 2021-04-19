240862e26a0c38e744b5e9c7c2445c408cb9bb90
uaaeu <yurtmusa@gmail.com>
2021-04-19 18:30:42 +0300

    alert prompt and confirm

---
+++ alertPropmtConfirm.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// prompt
+let age = prompt('how old are you?', 33);
+
+// alert
+alert(`you are ${age} years old!`);
+
+// confirm
+let admin = confirm('are you an admin?');
+alert(admin);
