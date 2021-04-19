281af69ca4ba7a5cb15b5c43e1e206bb2af8bc03
uaaeu <yurtmusa@gmail.com>
2021-04-19 16:32:15 +0300

    moved variables to seperate file

--- alert.js
+++ alert.js
@@ -5,23 +5,3 @@ alert('hello i am javascript!');
 /* multiple line
 comment
 */
-
-// VARIABLES
-// let
-let admin;
-let user;
-
-user = 'musa';
-
-admin = user;
-
-alert(admin);
-
-let ourPlanetName = 'earth';
-
-let currentUserName = 'uaaeu';
-
-// constant
-const BIRTHDATE = '13.12.1988';
-// const age = someCode(BIRTHDATE);
-
---
+++ variables.js
@@ -0,0 +1,20 @@
+'use strict';
+
+// VARIABLES
+// let
+let admin;
+let user;
+
+user = 'musa';
+
+admin = user;
+
+alert(admin);
+
+let ourPlanetName = 'earth';
+
+let currentUserName = 'uaaeu';
+
+// constant
+const BIRTHDATE = '13.12.1988';
+// const age = someCode(BIRTHDATE);
