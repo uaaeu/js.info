c8a1e1e9bf3803d8b99aa8ef17faa137bafd02f4
uaaeu <yurtmusa@gmail.com>
2021-04-19 16:52:45 +0300

    data types

---
+++ dataTypes.js
@@ -0,0 +1,37 @@
+'use strict'
+
+// DATA TYPES
+// number
+let number = 5;
+let float = 5.25;
+
+// bigint
+let bigInt = 654657986543215649876543165476894654n;
+
+// string
+let string = "musa";
+let stringSingleQuote = 'uaaeu';
+let stringOppositeQuote = `hello ${string}`;
+
+// boolean
+let trueBool = true;
+let falseBool = false;
+
+trueBool = 5 > 4;
+falseBool =  5 < 4;
+
+// null
+let nullDataType = null;
+
+// undefined
+let undefinedDataType;
+undefinedDataType = undefined;
+
+// typeof operator
+typeof 5; // 'number'
+typeof 'musa'; // 'string'
+typeof true; // 'boolean'
+typeof undefinedDataType; // 'undefined'
+typeof Math; // 'object'
+typeof Symbol('id'); // 'symbol'
+typeof alert; // 'function'
