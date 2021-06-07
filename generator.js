07a2c2f472a6dd5c1d1704175b77af64a5720239
uaaeu <yurtmusa@gmail.com>
2021-06-07 18:48:53 +0300

    generator

---
+++ generator.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function* pseudoRandom(seed) {
+  let value = seed;
+
+  while(true) {
+    value = value * 16807 % 2147483647;
+    yield value;
+  }
+}
+
+let generator = pseudoRandom(1);
+
+alert(generator.next().value);
+alert(generator.next().value);
+alert(generator.next().value);
